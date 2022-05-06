18bcd6a8cc11464cf09091aeabb6688591358441
Kevin Quing <willardquing@gmail.com>
2022-05-06 23:27:46 +0800

    Calculator - add some validations

--- components/calculator/calcButtonGroup.tsx
+++ components/calculator/calcButtonGroup.tsx
@@ -1,32 +1,69 @@
-const calcButtonClasses = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+import { MouseEvent } from 'react';
+export type CalcButtonGroupProps = {
+  onCalcBtnClick: (key: string) => void
+}
 
-const calcButtonChars = [
-  <button type='button' className={calcButtonClasses} key={'M C'}>M C</button>,
-  <button type='button' className={calcButtonClasses} key={'M +'}>M +</button>,
-  <button type='button' className={calcButtonClasses} key={'M -'}>M -</button>,
-  <button type='button' className={calcButtonClasses} key={'C'}>C</button>,
-  <button type='button' className={calcButtonClasses} key={7}>7</button>,
-  <button type='button' className={calcButtonClasses} key={8}>8</button>,
-  <button type='button' className={calcButtonClasses} key={9}>9</button>,
-  <button type='button' className={calcButtonClasses} key={'/'}>/</button>,
-  <button type='button' className={calcButtonClasses} key={4}>4</button>,
-  <button type='button' className={calcButtonClasses} key={5}>5</button>,
-  <button type='button' className={calcButtonClasses} key={6}>6</button>,
-  <button type='button' className={calcButtonClasses} key={'x'}>x</button>,
-  <button type='button' className={calcButtonClasses} key={1}>1</button>,
-  <button type='button' className={calcButtonClasses} key={2}>2</button>,
-  <button type='button' className={calcButtonClasses} key={3}>3</button>,
-  <button type='button' className={calcButtonClasses} key={'-'}>-</button>,
-  <button type='button' className={calcButtonClasses} key={'.'}>.</button>,
-  <button type='button' className={calcButtonClasses} key={0}>0</button>,
-  <button type='button' className={calcButtonClasses} key={'='}>=</button>,
-  <button type='button' className={calcButtonClasses} key={'+'}>+</button>,
-]
-export default function CalcButtonGroup() {
-  
+const calcButtonClasses = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-4xl'
+
+
+export default function CalcButtonGroup({ onCalcBtnClick }: CalcButtonGroupProps) {
+  const handleCalcPress = (e: MouseEvent<HTMLButtonElement>) => onCalcBtnClick(e?.currentTarget?.value)
   return (
     <div className="grid grid-cols-4 gap-4">
-      {calcButtonChars}
+      <div></div>
+      <div></div>
+      <div></div>
+      <button type='button' className={calcButtonClasses}
+        value="C" key={'C'}
+        onClick={handleCalcPress}>C</button>
+      <button type='button' className={calcButtonClasses}
+        value="7" key={7}
+        onClick={handleCalcPress}>7</button>
+      <button type='button' className={calcButtonClasses}
+        value="8" key={8}
+        onClick={handleCalcPress}>8</button>
+      <button type='button' className={calcButtonClasses}
+        value="9" key={9}
+        onClick={handleCalcPress}>9</button>
+      <button type='button' className={calcButtonClasses}
+        value="/" key={'/'}
+        onClick={handleCalcPress}>/</button>
+      <button type='button' className={calcButtonClasses}
+        value="4" key={4}
+        onClick={handleCalcPress}>4</button>
+      <button type='button' className={calcButtonClasses}
+        value="5" key={5}
+        onClick={handleCalcPress}>5</button>
+      <button type='button' className={calcButtonClasses}
+        value="6" key={6}
+        onClick={handleCalcPress}>6</button>
+      <button type='button' className={calcButtonClasses}
+        value="x" key={'x'}
+        onClick={handleCalcPress}>x</button>
+      <button type='button' className={calcButtonClasses}
+        value="1" key={1}
+        onClick={handleCalcPress}>1</button>
+      <button type='button' className={calcButtonClasses}
+        value="2" key={2}
+        onClick={handleCalcPress}>2</button>
+      <button type='button' className={calcButtonClasses}
+        value="3" key={3}
+        onClick={handleCalcPress}>3</button>
+      <button type='button' className={calcButtonClasses}
+        value="-" key={'-'}
+        onClick={handleCalcPress}>-</button>
+      <button type='button' className={calcButtonClasses}
+        value="." key={'.'}
+        onClick={handleCalcPress}>.</button>
+      <button type='button' className={calcButtonClasses}
+        value="0" key={0}
+        onClick={handleCalcPress}>0</button>
+      <button type='button' className={calcButtonClasses}
+        value="=" key={'='}
+        onClick={handleCalcPress}>=</button>
+      <button type='button' className={calcButtonClasses}
+        value="+" key={'+'}
+        onClick={handleCalcPress}>+</button>
     </div>
   )
 }
--- components/calculator/calculator.tsx
+++ components/calculator/calculator.tsx
@@ -1,9 +1,49 @@
+import { useState } from 'react';
+import { pipe } from 'ramda'
 import CalcButtonGroup from './calcButtonGroup';
+// move to model and make alias for string or specific strings type for calc
+const validators = ['+','-','x','/']
+// make recursive?
+const getLastNumberStr = (calcValue: string) => {
+  let i = calcValue.length
+  let str = ''
+  while(i--) {
+    if(validators.indexOf(calcValue[i]) > -1) {
+      break
+    } else {
+      str += calcValue[i]
+    }
+  }
+  return str
+}
+const getLastChar = (calcValue: string) => calcValue[calcValue.length - 1]
+const charIsValidator = (char:string) => validators.indexOf(char) > -1
+const lastCharIsValidator = pipe(getLastChar,charIsValidator)
+const hasDecimal = (value: string) => value.includes('.')
+const lastValueHasDecimal = pipe(getLastNumberStr, hasDecimal)
+const calcInputIsValid = (incoming: string, calcValue: string) => {
+  if(lastCharIsValidator(calcValue) && charIsValidator(incoming)) {
+    return false
+  }
+  if(lastValueHasDecimal(calcValue) && incoming === '.') {
+    return false
+  }
+  return true
+}
 
 export default function Calculator() {
+  const [calcInput, setCalcInput] = useState('')
+  const append = (key: string) => {
+    if(key === 'C') {
+      setCalcInput('')
+    } else if(calcInputIsValid(key, calcInput)) {
+      setCalcInput(calcInput + key)
+    } 
+  }
   return (
     <div className='flex flex-col'>
-      <CalcButtonGroup />
+      <div>{calcInput}</div>
+      <CalcButtonGroup onCalcBtnClick={append}/>
     </div>
   )
 }
